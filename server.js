ad6a15b969b4cba450a627d3ca07d1998cf7130c
DuctnNetwork <trannhuduc531998@gmail.com>
2020-01-22 01:38:08 +0700

    Fix port var

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
